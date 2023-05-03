be99a37749947e42797e8823d2beba36aee83440
mon1gold <daniildocenko218@gmail.com>
2023-05-03 19:26:59 +0200

    add homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       ValueCalculator valueCalculator = new ValueCalculator(1_000_000);
+        valueCalculator.calculator();
     }
 }
